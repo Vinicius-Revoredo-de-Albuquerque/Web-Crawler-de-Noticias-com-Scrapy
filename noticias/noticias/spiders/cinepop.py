cb74a82a3320c7b71b7a35a64362ae3125942995
Vinicius Revoredo <viniciusrevoredo@gmail.com>
2022-03-10 16:37:56 -0300

    Rectification in the Second Spider

--- noticias/noticias/spiders/cinepop.py
+++ noticias/noticias/spiders/cinepop.py
@@ -18,26 +18,26 @@ class CinepopSpider(scrapy.Spider):
         title = response.css("h1.entry-title::text").get()
         author = response.css("div.td-post-author-name a::text").get()
         date = response.css("span.td-post-date time::text").get()
-
-        preText = response.css("div.td-post-content.tagdiv-type p::text").getall()
-        text = "".join(preText)
+        text = "".join(response.css("div.td-post-content.tagdiv-type p::text").getall())
         link = response.url
         tags = response.css("div.td-post-source-tags li a::text").getall()
 
         # Métricas
         quantTags = len(tags)
-        time = str(self.tempoDeLeitura(preText)) + " min de leitura"
+        time = str(self.tempoDeLeitura(text)) + " min de leitura"
 
         dados = NoticiasItem(title=title, author=author, date=date, text=text, link=link, time=time, tags=tags, quantTags=quantTags)
         yield dados
 
 
-    def tempoDeLeitura(self, palavras):
+    def tempoDeLeitura(self, text):
         # Estipula-se que o tempo medio para leitura de cada palavra é em torno de 0.39 s
         # Portanto:
 
+        palavras = text.split()
+
         quantPalavras = len(palavras)
         tempoSegundos = float(quantPalavras * 0.39)
-        tempoMinutos = int(tempoSegundos / 2.0)
+        tempoMinutos = int(tempoSegundos / 60.0)
 
         return tempoMinutos
